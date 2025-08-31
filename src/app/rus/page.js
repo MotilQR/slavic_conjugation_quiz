c8e1606be8083508acd16c4ce4dee6ee76d71a05
MotilQR <pantenkov08@bk.ru>
2025-08-31 21:08:46 +0300

    Design update, new answers algorithm

--- src/app/rus/page.js
+++ src/app/rus/page.js
@@ -3,16 +3,49 @@ import en from "@/app/locales/en.json"
 import { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-function getThreeUniqueNumbers(exclude) {
-  const numbers = [1, 2, 3, 4, 5].filter(n => n !== exclude);
-  const result = [];
+function getThreeUniqueNumbers(exclude, pl) {
+    let numbers;
+    if (pl) {
+        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].filter(n => n !== exclude);
+    } else {
+        numbers = [1, 2, 3, 4, 5].filter(n => n !== exclude);
+    } 
+    const result = [];
 
-  while (result.length < 3) {
-    const index = Math.floor(Math.random() * numbers.length);
-    result.push(numbers.splice(index, 1)[0]);
-  }
+    while (result.length < 3) {
+        const index = Math.floor(Math.random() * numbers.length);
+        result.push(numbers.splice(index, 1)[0]);
+    }
 
-  return result;
+    return result;
+}
+
+function makeAnswersNoun(word, ans) {
+    const pl = word.forms.length > 6;
+    let falseCases = getThreeUniqueNumbers(ans, pl);
+    let answers = [];
+
+    let j = 0;
+    const a = Math.floor(Math.random() * 3);
+    for (let i = 0; i < 4; i++) {
+        if (i === a) {
+            answers.push({id: i, ans: word.forms[ans].form, isCorrect: true})
+        } else {
+            while (word.forms[ans].form === word.forms[falseCases[j]].form) {
+                falseCases[j] = (falseCases[j] + 1) % (pl ? 12 : 6);
+                falseCases[j] += (falseCases[j] == 0 ? 1 : 0);
+            }
+
+            if (answers.find((ans) => ans.ans === word.forms[falseCases[j]].form)) {
+                j++;
+            } else {
+                answers.push({id: i, ans: word.forms[falseCases[j]].form, isCorrect: false});
+                j++;
+            }
+        }
+    }
+
+    return answers;
 }
 
 export default function Rus() {
@@ -25,6 +58,8 @@ export default function Rus() {
 
     const [answered, setAnswered] = useState(false);
     const [correct, setCorrect] = useState(false);
+
+    const [loading, setLoading] = useState(false);
 
     const [answers, setAnswers] = useState([]);
 
@@ -70,22 +105,12 @@ export default function Rus() {
                 setWordCase(l.cases.locative)
                 break;
         }
-        let ans = [];
-        const a = Math.floor(Math.random() * 3);
-        //const falseCases = getThreeUniqueNumbers(cs, data[randId].forms.length > 6);
-        const falseCases = getThreeUniqueNumbers(cs);
-        let j = 0;
-        for (let i = 0; i < 4; i++) {
-            if (i == a) {
-                ans.push({id: i, ans: data[randId].forms[cs].form, isCorrect: true})
-            } else {
-                if (data[randId].forms[cs].form !== data[randId].forms[falseCases[j]].form)
-                    ans.push({id: i, ans: data[randId].forms[falseCases[j]].form, isCorrect: false})
-                j++;
-            }
-        }
-        setAnswers(ans);
-        setWord(pl ? data[randId].forms[6].form : data[randId].lemma)
+
+        setAnswers(makeAnswersNoun(data[randId], cs));
+        setAnswered(false);
+        setCorrect(false);
+        setLoading(false);
+        setWord(data[randId].lemma)
     }
 
     return (
@@ -94,7 +119,7 @@ export default function Rus() {
                 <h2 className="text-4xl font-extrabold mb-4">{l.russian.title}</h2>
                 <p className="text-lg mb-8">{l.russian.subtitle}</p>
 
-                <div className="wt-1 wb-1 border-b-2 border-[#DB2B39] w-1/2 flex gap-5 transition-all duration-1000 mt-4">
+                <div className="justify-center wt-1 wb-1 border-b-2 border-[#DB2B39] w-2/3 flex gap-5 transition-all duration-1000 mt-4">
                     <button
                         className={`text-lg pr-2 pl-2 rounded-t-lg transition-colors duration-500 ${Number(page) == Number(1) ? "bg-[#DB2B39]" : "hover:text-[#DB2B39] "}`}
                         onClick={() => setPage(1)}
@@ -135,12 +160,11 @@ export default function Rus() {
                             <button
                                 className="flex-1 font-semibold mt-4 px-6 py-1 bg-[#DB2B39] text-white rounded-xl shadow hover:bg-[#AE1E2A] transition"
                                 onClick={() => {
-                                    setAnswered(false)
-                                    setCorrect(false)
-                                    setWord(null)
+                                    setLoading(true)
+                                    fetchWord("NOUN")
                                 }}
                             >
-                                {l.russian.noButton}
+                                {loading ? l.russian.loading : l.russian.nextWord}
                             </button>
 
                             <button
@@ -148,15 +172,15 @@ export default function Rus() {
                                 onClick={() => {
                                     setAnswered(false)
                                     setCorrect(false)
-                                    fetchWord("NOUN")
+                                    setWord(null)
                                 }}
                             >
-                                {l.russian.nextWord}
+                                {l.russian.noButton}
                             </button>
                         </div>
                     </div>
                 ) : word ? (
-                        <div className="mt-2 flex flex-col items-start gap-4 w-1/2 ml-4">
+                        <div className="mt-2 flex flex-col items-start gap-4 ml-4">
                             <h1 className="text-xl font-bold p-0.5">{word}</h1>
                             <p>{l.russian.choose + (plural ? l.plurality.plural : l.plurality.single) + wordCase}</p>
                             {String(mode) === String("test") ? (
@@ -164,7 +188,7 @@ export default function Rus() {
                                     {answers.map(answer => (
                                         <button
                                             key={answer.id}
-                                            className="mt-4 px-6 py-3 bg-[#DB2B39] text-white rounded-xl shadow hover:bg-[#AE1E2A] transition"
+                                            className="mt-4 px-6 py-2 bg-[#29335C] text-white rounded-xl shadow hover:bg-[#DB2B39] transition"
                                             onClick={() => {
                                                 if (answer.isCorrect)
                                                     setCorrect(true);
@@ -184,7 +208,7 @@ export default function Rus() {
                             )}
                         </div>
                     ) : (
-                        <div className="mt-2 flex flex-col items-start gap-4 w-1/2 ml-4">
+                        <div className="mt-2 flex flex-col items-start gap-4 ml-4">
                             {page === 1 ? (
                                 <div className="flex flex-col items-start">
                                     <h1 className="text-xl font-bold p-0.5">{l.russian.noun}</h1>
@@ -215,9 +239,12 @@ export default function Rus() {
 
                                     <button
                                         className="mt-4 px-6 py-3 bg-[#DB2B39] text-white rounded-xl shadow hover:bg-[#AE1E2A] transition"
-                                        onClick={() => fetchWord("NOUN")}
+                                        onClick={() => {
+                                            fetchWord("NOUN")
+                                            setLoading(true)
+                                        }}
                                     >
-                                        {l.russian.load}
+                                        {loading ? l.russian.loading : l.russian.load}
                                     </button>
                                 </div>
                             ) : null}
